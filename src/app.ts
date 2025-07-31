9fe19c4e9d9fb6717b90cb9b2e6fc57ac501658f
ALIF <sayedalif18@gmail.com>
2025-07-31 13:04:06 +0600

    course first part done

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,18 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import { router } from './routes';
+import { globalErrorHandler } from './app/middlewares/globalErrorHandler';
+import notFound from './app/middlewares/notFound';
 
 const app = express();
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 app.use('/api/v1', router);
 
+app.use(globalErrorHandler);
+
+app.use(notFound);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to the Tour Management System API');
